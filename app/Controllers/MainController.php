68bcbbacbbe900a3263fecaa91a08a6b550ddd78
Nicolle Gutierrez <130277462+nicsxxgutierrez@users.noreply.github.com>
2023-09-22 19:13:21 +0800

    Create MainController.php

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\studModel;
+class MainController extends BaseController
+{
+    public function delete($data)
+    {
+        $main = new studModel();
+        $main->delete($data);
+        return redirect()->to('/test');
+    }
+    
+    
+    public function updates(){
+  
+        $data =[
+            'StudName' => $this->request->getPost('StudName'),
+            'StudGender' => $this->request->getPost('StudGender'),
+            'StudCourse' => $this->request->getPost('StudCourse'),
+            'StudSection' => $this->request->getPost('StudSection'),
+            'StudYear' => $this->request->getPost('StudYear'),
+            'Section' => $this->request->getPost('Section'),
+        ];
+        $main = new studModel();
+        $main->set($data)->where($data)->update();
+        return redirect()->to('/test');
+    }
+    public function update($ID)
+    {
+        $main = new studModel();
+        
+        $data = [ 
+            'main' => $main -> findAll(),
+            'd'=> $main->find($ID),
+        ];
+        return view('main', $data);
+    }
+
+    public function save()
+    {
+        $ID = $this->request->getPost('ID');
+        $data = [
+            'StudName' => $this->request->getPost('StudName'),
+            'StudGender' => $this->request->getPost('StudGender'),
+            'StudCourse' => $this->request->getPost('StudCourse'),
+            'StudSection' => $this->request->getPost('StudSection'),
+            'StudYear' => $this->request->getPost('StudYear'),
+            'Section' => $this->request->getPost('Section'),
+        ];
+        
+        $main = new studModel();
+
+        if (!empty($ID)){
+            $main->update($ID, $data);
+        }
+        else 
+        {
+            $main->save($data);
+        }
+
+        return redirect()->to('/test');
+    }
+    public function test()
+    {
+        $main = new studModel();
+        $data['main'] = $main->findAll();
+        return view ('main', $data);
+    }
+    public function index()
+    {
+        //
+    }
+}
